b5d80237b5301b45e20b9111bbb9bced7b76884d
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2025-09-02 16:08:27 +0300

    add length validation in dto

--- backend/src/comments/dto/create-comment.dto.ts
+++ backend/src/comments/dto/create-comment.dto.ts
@@ -1,11 +1,15 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class CreateCommentDto {
   @IsNotEmpty()
   @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   author: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(5)
+  @MaxLength(500)
   text: string;
 }
--- backend/src/posts/dto/create-post.dto.ts
+++ backend/src/posts/dto/create-post.dto.ts
@@ -1,15 +1,21 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class CreatePostDto {
   @IsNotEmpty()
   @IsString()
+  @MinLength(3)
+  @MaxLength(100)
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(10)
+  @MaxLength(2000)
   content: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   author: string;
 }
